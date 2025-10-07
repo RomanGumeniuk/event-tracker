99ed9ff1e6e81f6cb40fca1642bc3519274a1ec3
RomanGumeniuk <Vertonghengod@gmail.com>
2025-10-07 14:39:25 +0200

    changed structure

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-requireq('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
