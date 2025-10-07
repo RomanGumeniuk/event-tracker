cc70ee12b322e15778ff74f97bebbbd403028a44
RomanGumeniuk <Vertonghengod@gmail.com>
2025-10-07 23:44:23 +0200

    Integrate Tailwind CSS and add custom Input component
    Replaces the default CSS with Tailwind CSS, adds Tailwind and related dependencies, and introduces a reusable Input component. Updates App.tsx to use the new Input and applies a gradient background. Removes App.css and updates configuration files for Tailwind and PostCSS.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import { useState } from "react";
+import { Input } from "./components/ui/input";
 
 function App() {
-  return <></>;
+  const [name, setName] = useState("");
+
+  return (
+    <div
+      className="flex flex-col justify-center items-center h-screen font-lato"
+      style={{
+        background:
+          "radial-gradient(circle, rgba(63, 94, 251, 1) 0%, rgba(252, 70, 107, 1) 100%)",
+      }}
+    >
+      <Input
+        className="w-1/2 h-1/12 border-2 border-gray-300 rounded-lg border-radius-md p-2 text-center text-lg"
+        placeholder="Enter your name"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            setName(e.currentTarget.value);
+            e.currentTarget.value = "";
+          }
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
